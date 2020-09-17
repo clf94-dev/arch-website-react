8ff59ecc62e21425d0d7abf2a43d73c69a9b6c13
clf94-dev <clf94@hotmail.es>
2020-09-17 10:04:48 +0200

    navbar desktop style added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,33 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom';
 import Logo from './images/logo.svg'
-
+import './styles/App.scss'
 function Navbar() {
 
-const [click, setClick] =useState(false);
+    const [click,
+        setClick] = useState(false);
 
-const handleClick =() => setClick(!click);
+    const handleClick = () => setClick(!click);
 
     return (
         <nav className="navbar">
             <div className="navbar-cont">
-<img src={Logo} alt="arch"/>
-<div className="menu-icon" onClick={handleClick}>
-<i className={click? 'fas fa-times':'fas fa-bars'}></i>
-</div>
-<ul className={click? 'nav-menu active':'nav-menu'}>
-    <li className="nav-item">Portfolio</li>
-    <li className="nav-item">About Us</li>
-    <li className="nav-item">Careers</li>
+                <img src={Logo} alt="arch" />
+                <div className="menu-icon" onClick={handleClick}>
+                    <i
+                        className={click
+                        ? 'fas fa-times'
+                        : 'fas fa-bars'}></i>
+                </div>
+                <ul
+                    className={click
+                    ? 'nav-menu active'
+                    : 'nav-menu'}>
+                    <li className="nav-item">Portfolio</li>
+                    <li className="nav-item">About Us</li>
+                    <li className="nav-item">Careers</li>
 
-
-</ul>
+                </ul>
             </div>
         </nav>
     )
