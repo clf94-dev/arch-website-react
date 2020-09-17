2416639dcdb5d81d04d4284a2d5299134d97c60b
clf94-dev <clf94@hotmail.es>
2020-09-17 20:26:48 +0200

    added style to home intro section

--- src/components/HomeIntro.jsx
+++ src/components/HomeIntro.jsx
@@ -1,7 +1,23 @@
 import {Grid} from '@material-ui/core'
 import React, {useState} from 'react'
 import {HomeIntroData} from './HomeIntroData'
-import {ParamourD, ParamourT, ParamourM, TrinityD, TrinityT, TrinityM, SeraphD, SeraphM, SeraphT, FederalD, FederalM, FederalT} from './HomeIntroImg'
+import {
+    ParamourD,
+    ParamourT,
+    ParamourM,
+    TrinityD,
+    TrinityT,
+    TrinityM,
+    SeraphD,
+    SeraphM,
+    SeraphT,
+    FederalD,
+    FederalM,
+    FederalT
+} from './HomeIntroImg'
+
+import './styles/App.css'
+
 
 function HomeIntro() {
     const [project,
@@ -12,11 +28,16 @@ function HomeIntro() {
     const goToProject3 = () => setProject(2);
     const goToProject4 = () => setProject(3);
 
-    const imgD = [ParamourD, SeraphD,FederalD , TrinityD];
+    const imgD = [ParamourD, SeraphD, FederalD, TrinityD];
     return (
-        <Grid container>
-            <Grid container direction='row'>
+        <div>
+            
+            <Grid container direction='row' className='intro-cont'>
+               
                 <Grid item xs={12}>
+                    <img src={imgD[project]} alt={HomeIntroData[project].imgALT}/>
+                </Grid>
+                <Grid item xs={12} className='text-col'>
                     <h1>{HomeIntroData[project].title}</h1>
                     <p>
                         {HomeIntroData[project].text}
@@ -24,17 +45,23 @@ function HomeIntro() {
 
                     <button>See Our Portfolio</button>
                 </Grid>
-                <Grid item xs={12}>
-                    <img src={imgD[project]} alt={HomeIntroData[project].imgALT}/>
-                </Grid>
             </Grid>
-            <Grid container direction='row'>
-                <div className="btn" onClick={goToProject1}>01</div>
-                <div className="btn" onClick={goToProject2}>02</div>
-                <div className="btn" onClick={goToProject3}>03</div>
-                <div className="btn" onClick={goToProject4}>04</div>
+            <Grid container direction='row' className='btn-row'>
+                <div className="btn" onClick={goToProject1} style={{backgroundColor: project===0? 'black':'white', color:project===0? 'white':'black' }}>
+                    <h3>01</h3>
+                </div>
+                <div className="btn" onClick={goToProject2}style={{backgroundColor: project===1? 'black':'white', color:project===1? 'white':'black' }}>
+                    <h3>02</h3>
+                </div>
+                <div className="btn" onClick={goToProject3} style={{backgroundColor: project===2? 'black':'white', color:project===2? 'white':'black' }}>
+                    <h3>03</h3>
+                </div>
+                <div className="btn" onClick={goToProject4} style={{backgroundColor: project===3? 'black':'white', color:project===3? 'white':'black' }}>
+                    <h3>04</h3>
+                </div>
             </Grid>
-        </Grid>
+            
+        </div>
 
     )
 }
