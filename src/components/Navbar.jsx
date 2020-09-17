9dd07bae357fa85d063878c264958b11fff98d23
clf94-dev <clf94@hotmail.es>
2020-09-17 08:46:50 +0200

    basic navbar structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom';
+import Logo from './images/logo.svg'
 
 function Navbar() {
+
+const [click, setClick] =useState(false);
+
+const handleClick =() => setClick(!click);
+
     return (
-        <div>
-            
-        </div>
+        <nav className="navbar">
+            <div className="navbar-cont">
+<img src={Logo} alt="arch"/>
+<div className="menu-icon" onClick={handleClick}>
+<i className={click? 'fas fa-times':'fas fa-bars'}></i>
+</div>
+<ul className={click? 'nav-menu active':'nav-menu'}>
+    <li className="nav-item">Portfolio</li>
+    <li className="nav-item">About Us</li>
+    <li className="nav-item">Careers</li>
+
+
+</ul>
+            </div>
+        </nav>
     )
 }
 
