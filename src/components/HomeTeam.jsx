1c7504f8ca25f867497929d153fcd00a2d52a1eb
clf94-dev <clf94@hotmail.es>
2020-09-17 21:55:56 +0200

    added style home team (desktop)

--- src/components/HomeTeam.jsx
+++ src/components/HomeTeam.jsx
@@ -1,15 +1,18 @@
-import { Grid } from '@material-ui/core'
+import {Grid} from '@material-ui/core'
 import React from 'react'
 import Team from './images/home/desktop/image-small-team.jpg'
-
+import Arrow from './images/icons/icon-arrow.svg'
+import './styles/App.css'
 
 function HomeTeam() {
     return (
         <div>
-            <Grid container  direction='column'>
+            <Grid container direction='column' className='team-cont'>
                 <img src={Team} alt="team"/>
-                <h3>Small team, big ideas</h3>
-                <button>About Us</button>
+                <Grid container direction='column' className='team-text'>
+                    <h3>Small team, big ideas</h3>
+                    <button>About Us <img src={Arrow} alt="arrow"/></button>
+                </Grid>
             </Grid>
         </div>
     )
