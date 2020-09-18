ea0942cab1bde9e8ff94745a05ca6543d63d7c99
clf94-dev <clf94@hotmail.es>
2020-09-18 17:46:54 +0200

    added footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,26 @@
 import {Grid} from '@material-ui/core'
 import React from 'react'
 import LogoWhite from './images/logo copy.svg'
+import './styles/App.css'
+
 function Footer() {
     return (
-        <Grid container direction='row'>
-            <Grid item md={2} xs={12}><img src={LogoWhite} alt="Arch white"/></Grid>
-            <Grid item md={2} xs={12}>
-                <p>Portfolio</p>
-            </Grid>
-            <Grid item md={2} xs={12}>
-                <p>About Us</p>
-            </Grid>
-            <Grid item md={2} xs={12}>
-                <p>Careers</p>
-            </Grid>
-            <Grid item md={4} xs={12}>
-                <button>See Our Portfolio</button>
+        <Grid container direction='row' className='footer-cont'>
+            <Grid item md={2} xs={12} className='logo-cont'><img src={LogoWhite} alt="Arch white"/></Grid>
+           
+                <Grid item md={2} xs={12} className='links-col'>
+                    <p>Portfolio</p>
+                </Grid>
+
+                <Grid item md={2} xs={12} className='links-col'>
+                    <p>About Us</p>
+                </Grid>
+                <Grid item md={2} xs={12} className='links-col'>
+                    <p>Careers</p>
+                </Grid>
+                <Grid item md={4} xs={12} className='btn-col'>
+                    <button>See Our Portfolio</button>
+                
             </Grid>
 
         </Grid>
