a678a8636fdd067302df3bd30eb836bf635b2fc9
clf94-dev <clf94@hotmail.es>
2020-09-18 17:57:05 +0200

    removed text- decoration from links footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,21 +9,21 @@ function Footer() {
     return (
         <Grid container direction='row' className='footer-cont'>
             <Grid item md={2} xs={12} className='logo-cont'>
-                <Link to='/'>       <img src={LogoWhite} alt="Arch white"/></Link>
+                <Link to='/' style={{textDecoration:'none'}} >       <img src={LogoWhite} alt="Arch white"/></Link>
          </Grid>
            
                 <Grid item md={2} xs={12} className='links-col'>
-                 <Link to='/portfolio' ><p>Portfolio</p></Link>   
+                 <Link to='/portfolio' style={{textDecoration:'none'}} ><p>Portfolio</p></Link>   
                 </Grid>
 
                 <Grid item md={2} xs={12} className='links-col'>
-                <Link to='/about' > <p>About Us</p></Link>   
+                <Link to='/about' style={{textDecoration:'none'}} > <p>About Us</p></Link>   
                 </Grid>
                 <Grid item md={2} xs={12} className='links-col'>
-                <Link to='/careers' > <p>Careers</p></Link>   
+                <Link to='/careers' style={{textDecoration:'none'}}> <p>Careers</p></Link>   
                 </Grid>
                 <Grid item md={4} xs={12} className='btn-col'>
-                   <Link to='/portfolio'> <button>See Our Portfolio</button></Link>
+                   <Link to='/portfolio' style={{textDecoration:'none'}}> <button>See Our Portfolio</button></Link>
                 
             </Grid>
 
