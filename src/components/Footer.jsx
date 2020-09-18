277bce41ab36c5194edb558f26840a8638be1587
clf94-dev <clf94@hotmail.es>
2020-09-18 17:53:17 +0200

    added routes to footer elements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,29 @@
 import {Grid} from '@material-ui/core'
 import React from 'react'
+import {Link} from 'react-router-dom/cjs/react-router-dom.min';
+
 import LogoWhite from './images/logo copy.svg'
 import './styles/App.css'
 
 function Footer() {
     return (
         <Grid container direction='row' className='footer-cont'>
-            <Grid item md={2} xs={12} className='logo-cont'><img src={LogoWhite} alt="Arch white"/></Grid>
+            <Grid item md={2} xs={12} className='logo-cont'>
+                <Link to='/'>       <img src={LogoWhite} alt="Arch white"/></Link>
+         </Grid>
            
                 <Grid item md={2} xs={12} className='links-col'>
-                    <p>Portfolio</p>
+                 <Link to='/portfolio' ><p>Portfolio</p></Link>   
                 </Grid>
 
                 <Grid item md={2} xs={12} className='links-col'>
-                    <p>About Us</p>
+                <Link to='/about' > <p>About Us</p></Link>   
                 </Grid>
                 <Grid item md={2} xs={12} className='links-col'>
-                    <p>Careers</p>
+                <Link to='/careers' > <p>Careers</p></Link>   
                 </Grid>
                 <Grid item md={4} xs={12} className='btn-col'>
-                    <button>See Our Portfolio</button>
+                   <Link to='/portfolio'> <button>See Our Portfolio</button></Link>
                 
             </Grid>
 
