d9d820274ab7f4fe2049038981e4ead3c3f32b93
clf94-dev <clf94@hotmail.es>
2020-09-21 22:51:12 +0200

    added responsive style to top About & Contact

--- src/components/TopAboutContactCard.jsx
+++ src/components/TopAboutContactCard.jsx
@@ -4,13 +4,17 @@ import {TopInfo} from './TopInfo'
 
 import TopAboutD from './images/about/desktop/image-hero.jpg'
 import TopContactD from './images/contact/desktop/image-hero.jpg'
+import TopAboutT from './images/about/tablet/image-hero.jpg'
+import TopContactT from './images/contact/tablet/image-hero.jpg'
+import TopAboutM from './images/about/mobile/image-hero.jpg'
+import TopContactM from './images/contact/mobile/image-hero.jpg'
 
 function TopAboutContactCard(props) {
     return (
         <div>
             <Grid container direction='row' className='top-section-cont'>
                 <Grid item md={6} xs={12} className='img-col'>
-                    <img src={props.contact? TopContactD: TopAboutD} alt={props.contact
+                    <img src={props.contact? TopContactD: TopAboutD} srcset={props.contact? `${TopContactD} 1500w,${TopContactM} 756w,${TopContactT} 1080w `: `${TopAboutD} 1500w,${TopAboutM} 756w,${TopAboutT} 1080w `}  alt={props.contact
                             ? TopInfo[1].imgALT
                             : TopInfo[0].imgALT}/>
                 </Grid>
