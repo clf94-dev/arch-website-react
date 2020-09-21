27551b2e9bcef96cd0b3152d35121fd8f0c7798b
clf94-dev <clf94@hotmail.es>
2020-09-21 21:14:12 +0200

    added resposive size image to HomeTeam comp

--- src/components/HomeTeam.jsx
+++ src/components/HomeTeam.jsx
@@ -1,6 +1,8 @@
 import {Grid} from '@material-ui/core'
 import React from 'react'
 import Team from './images/home/desktop/image-small-team.jpg'
+import TeamT from './images/home/tablet/image-small-team.jpg'
+import TeamM from './images/home/mobile/image-small-team.jpg'
 import Arrow from './images/icons/icon-arrow.svg'
 import './styles/App.css'
 
@@ -10,12 +12,12 @@ function HomeTeam() {
     return (
         <div>
             <Grid container direction='column' className='team-cont'>
-                <img src={Team} alt="team"/>
+                <img src={Team} srcset={`${Team} 1500w, ${TeamM} 756w, ${TeamT} 1080w`} alt="team"/>
                 <Grid container direction='column' className='team-text'>
                     <h3>Small team, big ideas</h3>
                     <Link to='/about'>
                         <button>About Us
-                            <img src={Arrow} alt="arrow"/></button>
+                            <img src={Arrow}  alt="arrow"/></button>
                     </Link>
                 </Grid>
             </Grid>
