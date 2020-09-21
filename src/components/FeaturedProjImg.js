66d1485d7ce8c80555387e305ad93cd9776ecb71
clf94-dev <clf94@hotmail.es>
2020-09-21 21:34:42 +0200

    added responsive images to HomeFeatured section

--- src/components/FeaturedProjImg.js
+++ src/components/FeaturedProjImg.js
@@ -2,4 +2,13 @@ import SolFD from './images/portfolio/desktop/image-del-sol.jpg'
 import Tower228BD from './images/portfolio/desktop/image-228b.jpg'
 import PrototypePD from './images/portfolio/desktop/image-prototype.jpg'
 
-export { SolFD, Tower228BD, PrototypePD }
+import SolFT from './images/portfolio/tablet/image-del-sol.jpg'
+import Tower228BT from './images/portfolio/tablet/image-228b.jpg'
+import PrototypePT from './images/portfolio/tablet/image-prototype.jpg'
+
+
+import SolFM from './images/portfolio/mobile/image-del-sol.jpg'
+import Tower228BM from './images/portfolio/mobile/image-228b.jpg'
+import PrototypePM from './images/portfolio/mobile/image-prototype.jpg'
+
+export { SolFD, Tower228BD, PrototypePD, SolFM, SolFT, Tower228BM, Tower228BT, PrototypePM, PrototypePT }
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,16 +4,21 @@ import React from 'react'
 import {Grid} from '@material-ui/core';
 import {PortfolioProj } from './PortfolioProj'
 import { PrototypeD, PrototypeM, PrototypeT, ParamourD, ParamourT, ParamourM, TrinityD, TrinityT, TrinityM, SeraphD, SeraphM, SeraphT, FederalD, FederalM, FederalT, EdelweissD, EdelweissM, EdelweissT, EeboxD, EeboxM, EeboxT, SolD, SolM, SolT, Tower228D, Tower228M, Tower228T, NetcryD, NetcryT, NetcryM, HypersD, HypersT, HypersM, SXIVD, SXIVM, SXIVT } from './PortfolioProjImg'
-import  { SolFD,  Tower228BD, PrototypePD} from './FeaturedProjImg'
+import  { SolFD, Tower228BD, PrototypePD, SolFM, SolFT, Tower228BM, Tower228BT, PrototypePM, PrototypePT } from './FeaturedProjImg'
 import {Link} from 'react-router-dom/cjs/react-router-dom.min';
 
 function ProjectCard(props) {
     const FeaturedD = [SolFD, Tower228BD, PrototypePD];
+    const FeaturedT = [SolFT, Tower228BT, PrototypePT];
+    const FeaturedM = [SolFM, Tower228BM, PrototypePM];
     const PortfolioProjD = [SeraphD ,EeboxD, FederalD, SolD,PrototypeD, Tower228D,EdelweissD, NetcryD, HypersD, SXIVD,  TrinityD,  ParamourD];
+    const PortfolioProjT = [SeraphT ,EeboxT, FederalT, SolT,PrototypeT, Tower228T,EdelweissT, NetcryT, HypersT, SXIVT,  TrinityT,  ParamourT];
+    const PortfolioProjM = [SeraphM ,EeboxM, FederalM, SolD,PrototypeM, Tower228M,EdelweissM, NetcryM, HypersM, SXIVM,  TrinityM,  ParamourM];
+    
     return (
 
         <Grid className={props.portfolio?'project-port-card-cont': 'project-card-cont'} item lg={4} md={12}>
-            <img src={props.portfolio? PortfolioProjD[props.index]: FeaturedD[props.index]} alt={props.imgALT}/>
+            <img src={props.portfolio? PortfolioProjD[props.index]: FeaturedD[props.index]} srcset={props.portfolio? `${PortfolioProjD[props.index]} 1500w,${PortfolioProjT[props.index]} 1080w,${PortfolioProjM[props.index]} 756w `: `${FeaturedD[props.index]} 1500w, ${FeaturedM[props.index]} 756w, ${FeaturedT[props.index]} 1080w`} alt={props.imgALT}/>
             <h1>{props.projNum}</h1>
             <Grid container direction='column' className='project-card-text'>
                 <h3
