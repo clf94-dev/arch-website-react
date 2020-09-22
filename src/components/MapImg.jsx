9b3dd87b7462943fbfd92eefbd9e9ceb025d23af
clf94-dev <clf94@hotmail.es>
2020-09-22 08:37:47 +0200

    added variable sizes img to MapImg component

--- src/components/MapImg.jsx
+++ src/components/MapImg.jsx
@@ -1,13 +1,17 @@
 import { Grid } from '@material-ui/core'
 import React from 'react'
-import MapIMG from './images/contact/tablet/image-map.png'
+import MapIMG from './images/contact/desktop/image-map.png'
+
+import MapIMGT from './images/contact/tablet/image-map.png'
+import MapIMGM from './images/contact/mobile/image-map.png'
+
 import './styles/App.css'
 
 function MapImg() {
     return (
         <div>
             <Grid container  className='map-img-cont'>
-                <img src={MapIMG} alt="Map"/>
+                <img src={MapIMG} srcset={`${MapImg} 1500w, ${MapIMGT} 1080w ${MapIMGM} 756w`} alt="Map"/>
             </Grid>
         </div>
     )
