751aa26eb6d42a51b3d29b3a16f5b88effb0b27e
clf94-dev <clf94@hotmail.es>
2020-09-25 22:07:46 +0200

    added scroll aninmation to contact info section

--- src/components/ContactInfoCard.jsx
+++ src/components/ContactInfoCard.jsx
@@ -1,10 +1,14 @@
 import {Grid} from '@material-ui/core'
 import React from 'react'
 
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+AOS.init();
+
 function ContactInfoCard(props) {
     return (
         <Grid container direction='row' >
-            <Grid item lg={12} xs={12} className='info-card' >
+            <Grid item lg={12} xs={12} className='info-card' data-aos='fade-left' >
                 <p>{props.office}</p>
                 <p>
                     {props.mail}
--- src/components/MapImg.jsx
+++ src/components/MapImg.jsx
@@ -7,10 +7,14 @@ import MapIMGM from './images/contact/mobile/image-map.png'
 
 import './styles/App.css'
 
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+AOS.init();
+
 function MapImg() {
     return (
         <div>
-            <Grid container  className='map-img-cont'>
+            <Grid container  className='map-img-cont' data-aos='fade-up' data-aos-delay='300'>
                 <img src={MapIMG} srcset={`${MapImg} 1500w, ${MapIMGT} 1080w ${MapIMGM} 756w`} alt="Map"/>
             </Grid>
         </div>
