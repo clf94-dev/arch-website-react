f81803b11ae2dfb83032258b2735ad8b4e823e81
clf94-dev <clf94@hotmail.es>
2020-09-25 21:44:48 +0200

    added scroll animation team section home page

--- src/components/HomeTeam.jsx
+++ src/components/HomeTeam.jsx
@@ -8,10 +8,16 @@ import './styles/App.css'
 
 import {Link} from 'react-router-dom/cjs/react-router-dom.min';
 
+
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+AOS.init();
+
+
 function HomeTeam() {
     return (
         <div>
-            <Grid container direction='column' className='team-cont'>
+            <Grid container direction='column' className='team-cont' data-aos='fade-up' data-aos-delay='300'>
                 <img src={Team} srcset={`${Team} 1500w, ${TeamM} 756w, ${TeamT} 1080w`} alt="team"/>
                 <Grid container direction='column' className='team-text'>
                     <h3>Small team, big ideas</h3>
