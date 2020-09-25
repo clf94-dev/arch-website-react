03f9c847fd9dfa080154cbf6462e36a226ccff4b
clf94-dev <clf94@hotmail.es>
2020-09-25 22:11:27 +0200

    added animation to form inputs and button

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,6 +2,10 @@ import {Button, Grid} from '@material-ui/core'
 import React from 'react'
 import {useForm} from 'react-hook-form';
 
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+AOS.init();
+
 function Form() {
 
     const {handleSubmit , register, errors} = useForm();
@@ -14,7 +18,7 @@ function Form() {
                 <Grid item lg={4} xs={12} className='title-col'>
                     <h3>Connect with us</h3>
                 </Grid>
-                <Grid item lg={8} xs={12} className='form-cont'>
+                <Grid item lg={8} xs={12} className='form-cont' >
                     <form onSubmit={handleSubmit(onsubmit)} >
                         <input
                             type="text"
@@ -29,7 +33,9 @@ function Form() {
                                 borderBottomColor: "black"
                             }}
                             ref={register({required: true, maxLength: 30})}
-                            className={errors.name? "error input" :" input"}/>
+                            className={errors.name? "error input" :" input"}
+                            data-aos='fade-left'
+                            data-aos-delay='200'/>
 
                         <input
                             type="email"
@@ -44,7 +50,10 @@ function Form() {
                                 borderBottomColor: "black"
                             }}
                             ref={register({required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i})}
-                            className={errors.email? "error input" :" input"}/>
+                            className={errors.email? "error input" :" input"}
+                            data-aos='fade-left'
+                            data-aos-delay='400'
+                            />
                         <textarea
                             type="text"
                             name="message"
@@ -57,9 +66,12 @@ function Form() {
                             : {
                                 borderBottomColor: "black"
                             }}
-                            ref={register({required: true})}/>
+                            ref={register({required: true})} 
+                            data-aos='fade-left'
+                            data-aos-delay='600'
+                            />
                     </form>
-                    <Button type="submit" onClick={handleSubmit(onSubmit)} className='submit-btn'>
+                    <Button type="submit" onClick={handleSubmit(onSubmit)} className='submit-btn' data-aos='fade-up'  data-aos-delay='800'>
                     Send Message </Button>
 
                 </Grid>
